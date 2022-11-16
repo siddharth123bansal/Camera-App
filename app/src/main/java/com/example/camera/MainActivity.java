a32ad4b4848b864251d2111dedf27043995d5327
Siddharth Bansal <chinubansal766@gmail.com>
2022-11-16 21:17:48 +0530

    metadata integration

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -31,7 +31,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -39,6 +38,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     FusedLocationProviderClient loc;
     Calendar calendar;
     TextView txt;
-    String  finald,date;
+    public String  latitude,longitude,Country,city,finald="",date="",x="";
     public final static int REQUEST_CODE = 1034;
     Uri image_uri;
     @Override
@@ -64,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
         calendar=Calendar.getInstance();
         date=new SimpleDateFormat("dd-MM-yyyy, HH:mm", Locale.getDefault()).format(new Date());
         img=findViewById(R.id.IMG);
-
         loc= LocationServices.getFusedLocationProviderClient(this);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,42 +78,60 @@ public class MainActivity extends AppCompatActivity {
                        requestPermissions(per,REQUEST_CODE);
                    }else{
                        openCamera();
+                       Log.e("exe","first");
                        loc.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                           String las="";
                            @Override
                            public void onSuccess(Location location) {
                                 if(location!=null){
                                     Geocoder geo=new Geocoder(MainActivity.this, Locale.getDefault());
+                                    String ask = "";
                                     try {
                                         List<Address> address =geo.getFromLocation(location.getLatitude(),location.getLongitude(),1);
-                                        String latitude= String.valueOf(address.get(0).getLatitude());
-                                        String longitude= String.valueOf(address.get(0).getLongitude());
-                                        String Country=String.valueOf(address.get(0).getCountryName());
-                                        String city=String.valueOf(address.get(0).getLocality());
+                                         latitude= String.valueOf(address.get(0).getLatitude());
+                                         longitude= String.valueOf(address.get(0).getLongitude());
+                                         Country=String.valueOf(address.get(0).getCountryName());
+                                         city=String.valueOf(address.get(0).getLocality());
                                         finald= latitude+" "+longitude+"\n"+Country+" "+city+"\n"+date+"\n";
-
+                                        ask=finald.trim();
                                         txt.setText(finald);
-                                        Toast.makeText(MainActivity.this, finald, Toast.LENGTH_LONG).show();
-
-                                    } catch (IOException e) {
-                                        Toast.makeText(MainActivity.this, "Cant get The Location", Toast.LENGTH_SHORT).show();
                                     }
-                                }else{
-                                    Toast.makeText(MainActivity.this, "Cant get The Location", Toast.LENGTH_SHORT).show();
+                                    catch (IOException e)
+                                    {
+                                        Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                                    }
+                                    x=ask;
                                 }
+                                Log.e("Value",x);
+                               ContentValues values=new ContentValues();
+                               values.put(MediaStore.Images.Media.DISPLAY_NAME,x+"\n"+" img.jpg");
+                               values.put(MediaStore.Images.Media.MIME_TYPE,"images/*");
+                               values.put(MediaStore.Images.Media.DESCRIPTION,x);
+
+                               image_uri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
+                               Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                               intent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+                               startActivityForResult(intent,CAPTURE_CODE);;
                            }
+
                        });
+
+
                    }
                }else{
+                   Toast.makeText(MainActivity.this, "Else"+finald, Toast.LENGTH_SHORT).show();
                    openCamera();
                }
-
             }
         });
     }
     private void openCamera() {
         ContentValues values=new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE,"new Image");
-        values.put(MediaStore.Images.Media.DESCRIPTION,finald);
+        values.put(MediaStore.Images.Media.DISPLAY_NAME,System.currentTimeMillis()+".jpg");
+        values.put(MediaStore.Images.Media.MIME_TYPE,"images/*");
+        values.put(MediaStore.Images.Media.DESCRIPTION,123456789);
+
         image_uri=getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
 
         Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -130,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
             case REQUEST_CODE:
                 if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
                 {
+                    Toast.makeText(this, "PrePared", Toast.LENGTH_SHORT).show();
                     openCamera();
                 }
                 else
